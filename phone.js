c28e262acf5cfb59b0c7ea32fb7024459401497e
kirandhathri-b <boddepallik1@udayton.edu>
2024-01-29 12:32:03 -0500

    Assignment2
    Assignment2

--- phone.js
+++ phone.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
+
   function showTab(tabId) {
     $(".content_tab").hide();
     $("#" + tabId).show();
   }
 
-showTab("content_dialer");
+
+  showTab("content_dialer");
+
 
   $("#Dialpad").click(function() {
     showTab("content_dialer");
@@ -18,4 +21,50 @@ showTab("content_dialer");
   $("#Addcontacts").click(function() {
     showTab("content_addcontact");
   });
+
+  $("#testgestures").click(function() {
+    showTab("content_testgestures");
+  });
+
+
+  $("#content_dialer .row1 button, #content_dialer .row2 button, #content_dialer .row3 button, #content_dialer .row4 button").click(function() {
+    var num = $(this).text();
+    $("#numpad").val($("#numpad").val() + num);
+  });
+
+
+  $("#clear").click(function() {
+    $("#numpad").val("");
+  });
+
+
+  $("#dial").click(function() {
+    alert("Calling " + $("#numpad").val());
+  });
+
+  let downX = 0;
+  let downY = 0;
+  $("#gestureareaip").mousedown(function(event) {
+    downX = event.pageX;
+    downY = event.pageY;
+  });
+  $("#gestureareaip").mouseup(function(event) {
+    let upX = event.pageX;
+    let upY = event.pageY;
+
+    if (upX < downX){
+      $("#gestureop").val("swiped left");
+    }
+    else if (upX >downX){
+      $("#gestureop").val("swiped right");
+    }
+    //else if (upY > downY){
+    //  $("#gestureop").val("swiped down");
+    //}
+    //else if (upY < downY){
+    //  $("#gestureop").val("swiped up");
+    //}
+  });
+
+
 });
